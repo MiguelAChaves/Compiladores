d647fe77670bccae2959ecd6996cb7ed55bb46ad
Miguel Angel Chaves <miguelangelipn@outlook.com>
2019-09-01 17:11:42 -0500

    Integración frame principal
    Se añade la pantalla de inicio del compliador

---
+++ Proyecto/ver1.0/src/main/java/Utilities/main.java
@@ -0,0 +1,27 @@
+/**
+ * Mc Compiler                              |v1.0
+ * @author Miguel Angel Chaves
+ * 
+ * @desc: Compilador para la asignatura de Compiladores 20-1.
+ * 
+ * --- LOG DE VERSIONES ---
+ * v1.0     | Creación de Automatas AFN
+ *          | Operaciones: Union, Concatenación, Cerraduras
+ *          | Conversión de AFN a AFD
+ */
+package Utilities;
+import Vistas.*;
+import AFN.AFN;
+import java.util.LinkedList;
+public class main {
+    public static void main(String[] args) {
+        System.out.println("-------------------------------------------------");
+        System.out.println("    Mc. Compiler                    |   v1.0     ");
+        System.out.println("-------------------------------------------------");
+        //Leer AFN en archivos
+        LinkedList<AFN> ConjuntoAFN = new LinkedList<> ();
+        //Invocar al MenuPrincipal
+        MenuPrincipal m = new MenuPrincipal(ConjuntoAFN);
+        m.setVisible(true);
+    }
+}
