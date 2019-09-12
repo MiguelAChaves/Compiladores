6c2a1e644ba94069e643ca42a8a994f0c815ad43
Miguel Angel Chaves <miguelangelipn@outlook.com>
2019-09-11 23:28:57 -0500

    Modificación vista AFD
    Se agrega combobox para seleccionar el AFN a convertir

--- Proyecto/ver1.0/src/main/java/Vistas/generarAFD.java
+++ Proyecto/ver1.0/src/main/java/Vistas/generarAFD.java
@@ -64,6 +64,8 @@ public class generarAFD extends javax.swing.JFrame {
         jButton1 = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         tblAFN = new javax.swing.JTable();
+        jLabel1 = new javax.swing.JLabel();
+        cbAut_AFD = new javax.swing.JComboBox<>();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -84,6 +86,8 @@ public class generarAFD extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(tblAFN);
 
+        jLabel1.setText("Automata a Convertir");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -97,12 +101,22 @@ public class generarAFD extends javax.swing.JFrame {
                         .addGap(18, 18, 18)
                         .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 560, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap(18, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(0, 0, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addGap(26, 26, 26)
+                .addComponent(cbAut_AFD, javax.swing.GroupLayout.PREFERRED_SIZE, 92, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(186, 186, 186))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addGap(32, 32, 32)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(cbAut_AFD, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 376, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 32, Short.MAX_VALUE)
                 .addComponent(jButton1)
                 .addContainerGap())
@@ -117,7 +131,18 @@ public class generarAFD extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton1ActionPerformed
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JComboBox<String> cbAut;
+    private javax.swing.JComboBox<String> cbAut1;
+    private javax.swing.JComboBox<String> cbAut2;
+    private javax.swing.JComboBox<String> cbAut3;
+    private javax.swing.JComboBox<String> cbAut4;
+    private javax.swing.JComboBox<String> cbAut5;
+    private javax.swing.JComboBox<String> cbAut6;
+    private javax.swing.JComboBox<String> cbAut7;
+    private javax.swing.JComboBox<String> cbAut8;
+    private javax.swing.JComboBox<String> cbAut_AFD;
     private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tblAFN;
     // End of variables declaration//GEN-END:variables
